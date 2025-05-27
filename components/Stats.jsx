23471214c1a669fb67ad210ecba356f3222253d4
unknown <ionova.io@outlook.com>
2025-05-27 10:20:22 -0700

    Stats Commit

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,5 +1,60 @@
+"use client";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+import CountUp from "react-countup";
+
+const statsData = [
+  {
+    endCountNum: 99,
+    endCountText: "%",
+    text: "Clients Satisfaction",
+  },
+  {
+    endCountNum: 100,
+    endCountText: "+",
+    text: "Successful Projects",
+  },
+  {
+    endCountNum: 50,
+    endCountText: "+",
+    text: "Happy Clients",
+  },
+  {
+    endCountNum: 10,
+    endCountText: "+",
+    text: "Years of Experience",
+  },
+];
+
 const Stats = () => {
-  return <section>stats</section>;
+  const ref = useRef(null);
+  const inView = useInView(ref, { treshold: 0.2 });
+  return (
+    <div ref={ref} className="mt-16 xl:mt-32 bg-primary py-10 w-full">
+      <div className="container mx-auto h-full">
+        <div className="text-white flex flex-col items-center justify-between xl:flex-row h-full gap-12 text-center xl:text-left">
+          {statsData.map((item, index) => {
+            return (
+              <div className="w-full" key={index}>
+                <div className="text-5xl font-semibold">
+                  {inView && (
+                    <CountUp
+                      start={0}
+                      end={item.endCountNum}
+                      delay={0.5}
+                      duration={3}
+                    />
+                  )}
+                  <span>{item.endCountText}</span>
+                </div>
+                <p className="font-medium">{item.text}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
